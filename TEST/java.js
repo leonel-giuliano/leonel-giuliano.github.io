ed600891becc3929bd2153a6c11d4ffda91bb440
leonel-giuliano <leonelgiuliano7@gmail.com>
2023-08-18 15:37:52 -0300

    Remove for gitignore

--- TEST/java.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector(".input");
-const resultado = document.createElement("DIV");
-resultado.classList.add("resultado");
-
-input.addEventListener("keydown",(e)=>{
-    console.log(e);
-    if (input.value.includes(65)) resultado.innerHTML = "xd";
-})
